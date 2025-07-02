94e4023402a0255647a54f4bf53466318848a7b2
AbdullahAl <mamunprince595@gmail.com>
2025-07-02 13:37:59 +0530

    added more examples

--- program1.js
+++ program1.js
@@ -1,6 +1,53 @@
-//let num = 3.45632;
-//num = parseInt(num);
-//console.log(num.toFixed(5));
-
+let num = 3.45632;
+num = parseInt(num);
+console.log(num.toFixed(5));
 
 console.log(Number(false));
+
+let firstName = "Abdullah Al";
+let lastname = " Mamun";
+
+let fullName = firstName + lastname;
+console.log("The full name is " + fullName);
+
+let text = "Bangladesh";
+let len = text.length;
+
+console.log("The number of length is " + len);
+
+let text = prompt("Enter your name: ");
+let len = text.length;
+
+document.write("The number of length is " + text.length);
+
+let text = "bangladesh";
+document.write(text.charAt(1));
+
+let text = "bangladesh";
+text = text.toUpperCase();
+document.write(text);
+
+
+let text1 = "Bangladesh";
+let text2 = " is a beautiful country";
+let text = text1.concat(text2);
+document.write(text);
+
+let text1 = "Bangladesh";
+let text = text1.slice(3,7);
+document.write(text);
+
+const userName = "Abdullah Al mamun";
+const income = 897.90;
+const expense = 456.90;
+
+console.log(Personal budget tracker);
+console.log("User " + userName );
+console.log("Total income: " + income);
+console.log("Total expense: " + expense);
+
+
+
+
+
+
